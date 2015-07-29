33dd4407ca18c928bb274005c93d0dff19123278
ctukc <ctukc.nt@gmail.com>
2015-07-29 14:38:15 +0300

    новый проект

---
+++ UPPY.Desktop/Ninject/ApplicationModule.cs
@@ -0,0 +1,40 @@
+﻿using Core.DomainModel;
+using Core.Interfaces;
+using DesktopApp.Controllers;
+using DesktopApp.Infrastructure;
+using DesktopApp.Interfaces;
+using DesktopApp.Temp;
+using MongoDB.Driver;
+using MongoWork;
+using Ninject.Modules;
+
+namespace DesktopApp.Ninject
+{
+    public class ApplicationModule : NinjectModule
+    {
+        public override void Load()
+        {
+            var connection = new MongoDbConnection();
+            Bind(typeof(IMongoDatabase)).ToConstant(connection.Database);
+            Bind(typeof(IDataManagersFactory)).To(typeof(UppyFakeDataManagersFactory));
+
+            //Bind(typeof(IClassDataManager<Drawing>)).To(typeof(DrawingListClassFakeDataManager));
+            //Bind(typeof(IClassDataManager<TechRoute>)).To(typeof(TechRoutesClassFakeDataManager));
+            //Bind(typeof(IClassDataManager<Standart>)).To(typeof(StandartClassFakeDataManager));
+
+            Bind(typeof(IControllerFactory)).To(typeof(ControllersFactory));
+
+            //Bind(typeof(IController<Drawing>)).To(typeof(DrawingController));
+            //Bind(typeof(IController<HierarchyNumberDrawing>)).To(typeof(HierarchyNumberDrawingController));
+            //Bind(typeof(IController<Standart>)).To(typeof(StandartController));
+
+            Bind(typeof(IFormsBuilder)).To(typeof(FormsBuilder));
+
+            Bind(typeof (IController<TechOperation>)).To(typeof (Controller<TechOperation>));
+            Bind(typeof (IController<TechRoute>)).To(typeof (Controller<TechRoute>));
+
+
+
+        }
+    }
+}
