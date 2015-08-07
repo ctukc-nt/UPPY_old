957e93905f01347804aa28a624f27ae8c6dc0eb1
ctukc <ctukc.nt@gmail.com>
2015-08-07 11:39:42 +0300

    добавлен хэлпер

---
+++ Core/DrawingsToStandartHelper.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using Core.DomainModel;
+using Core.Interfaces;
+
+namespace Core
+{
+    public class DrawingsToStandartHelper
+    {
+
+        private double _density = 7.85;
+
+        /// <summary>
+        /// Плотность
+        /// </summary>
+        public double Density
+        {
+            get { return _density; }
+            set { _density = value; }
+        }
+
+        public bool CreateStandartByDrawing(Drawing headDrawing, IHierClassDataManager<Drawing> dataManager )
+        {
+            var allChildrens = dataManager.GetListAllChildrens(headDrawing.Id);
+            var allParentId = allChildrens.Select(x => x.ParentId);
+            var allDetails = allChildrens.Where(x => allParentId.Any(y => y.HasValue && y.Value == x.Id));
+
+        }
+
+        //public bool CreateStandartByDrawing(Drawing headDrawing, List<Drawing> drawings)
+        //{
+
+        //}
+    }
+}
