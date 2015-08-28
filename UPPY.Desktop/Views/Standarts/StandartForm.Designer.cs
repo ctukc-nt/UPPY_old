d5dd3572b03f083cc117a4e44670252f05e088fa
ctukc <ctukc.nt@gmail.com>
2015-08-28 10:34:21 +0300

    на форму редактирвоания норм добавлена таблица позиций

--- UPPY.Desktop/Views/Standarts/StandartForm.Designer.cs
+++ UPPY.Desktop/Views/Standarts/StandartForm.Designer.cs
@@ -29,10 +29,193 @@
         private void InitializeComponent()
         {
             this.components = new System.ComponentModel.Container();
+            this.gridControl1 = new DevExpress.XtraGrid.GridControl();
+            this.gridView1 = new DevExpress.XtraGrid.Views.Grid.GridView();
+            this.expandCollapsePanel1 = new MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel();
+            this.panel1 = new System.Windows.Forms.Panel();
+            this.positionStandartBindingSource = new System.Windows.Forms.BindingSource(this.components);
+            this.colProfile = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colStandartSize = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colGostOnSort = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colMarkSteal = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colUtilizationRatio = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colWeight = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colWeightWithWaste = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAdditionalMeasurement = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colAdditionalMeasurementWithWaste = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colTypeAdditionalMeasurement = new DevExpress.XtraGrid.Columns.GridColumn();
+            this.colNote = new DevExpress.XtraGrid.Columns.GridColumn();
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionStandartBindingSource)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // gridControl1
+            // 
+            this.gridControl1.DataSource = this.positionStandartBindingSource;
+            this.gridControl1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.gridControl1.Location = new System.Drawing.Point(0, 124);
+            this.gridControl1.MainView = this.gridView1;
+            this.gridControl1.Name = "gridControl1";
+            this.gridControl1.Size = new System.Drawing.Size(1107, 334);
+            this.gridControl1.TabIndex = 0;
+            this.gridControl1.ViewCollection.AddRange(new DevExpress.XtraGrid.Views.Base.BaseView[] {
+            this.gridView1});
+            // 
+            // gridView1
+            // 
+            this.gridView1.Columns.AddRange(new DevExpress.XtraGrid.Columns.GridColumn[] {
+            this.colProfile,
+            this.colStandartSize,
+            this.colGostOnSort,
+            this.colMarkSteal,
+            this.colUtilizationRatio,
+            this.colWeight,
+            this.colWeightWithWaste,
+            this.colAdditionalMeasurement,
+            this.colAdditionalMeasurementWithWaste,
+            this.colTypeAdditionalMeasurement,
+            this.colNote});
+            this.gridView1.GridControl = this.gridControl1;
+            this.gridView1.Name = "gridView1";
+            // 
+            // expandCollapsePanel1
+            // 
+            this.expandCollapsePanel1.ButtonSize = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonSize.Normal;
+            this.expandCollapsePanel1.ButtonStyle = MakarovDev.ExpandCollapsePanel.ExpandCollapseButton.ExpandButtonStyle.Circle;
+            this.expandCollapsePanel1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.expandCollapsePanel1.ExpandedHeight = 0;
+            this.expandCollapsePanel1.IsExpanded = true;
+            this.expandCollapsePanel1.Location = new System.Drawing.Point(0, 0);
+            this.expandCollapsePanel1.Name = "expandCollapsePanel1";
+            this.expandCollapsePanel1.Size = new System.Drawing.Size(1107, 124);
+            this.expandCollapsePanel1.TabIndex = 1;
+            this.expandCollapsePanel1.Text = "expandCollapsePanel1";
+            this.expandCollapsePanel1.UseAnimation = true;
+            // 
+            // panel1
+            // 
+            this.panel1.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.panel1.Location = new System.Drawing.Point(0, 458);
+            this.panel1.Name = "panel1";
+            this.panel1.Size = new System.Drawing.Size(1107, 41);
+            this.panel1.TabIndex = 2;
+            // 
+            // positionStandartBindingSource
+            // 
+            this.positionStandartBindingSource.DataSource = typeof(Core.DomainModel.PositionStandart);
+            // 
+            // colProfile
+            // 
+            this.colProfile.FieldName = "Profile";
+            this.colProfile.Name = "colProfile";
+            this.colProfile.Visible = true;
+            this.colProfile.VisibleIndex = 0;
+            // 
+            // colStandartSize
+            // 
+            this.colStandartSize.FieldName = "StandartSize";
+            this.colStandartSize.Name = "colStandartSize";
+            this.colStandartSize.Visible = true;
+            this.colStandartSize.VisibleIndex = 1;
+            // 
+            // colGostOnSort
+            // 
+            this.colGostOnSort.FieldName = "GostOnSort";
+            this.colGostOnSort.Name = "colGostOnSort";
+            this.colGostOnSort.Visible = true;
+            this.colGostOnSort.VisibleIndex = 2;
+            // 
+            // colMarkSteal
+            // 
+            this.colMarkSteal.FieldName = "MarkSteal";
+            this.colMarkSteal.Name = "colMarkSteal";
+            this.colMarkSteal.Visible = true;
+            this.colMarkSteal.VisibleIndex = 3;
+            // 
+            // colUtilizationRatio
+            // 
+            this.colUtilizationRatio.FieldName = "UtilizationRatio";
+            this.colUtilizationRatio.Name = "colUtilizationRatio";
+            this.colUtilizationRatio.Visible = true;
+            this.colUtilizationRatio.VisibleIndex = 4;
+            // 
+            // colWeight
+            // 
+            this.colWeight.FieldName = "Weight";
+            this.colWeight.Name = "colWeight";
+            this.colWeight.Visible = true;
+            this.colWeight.VisibleIndex = 5;
+            // 
+            // colWeightWithWaste
+            // 
+            this.colWeightWithWaste.FieldName = "WeightWithWaste";
+            this.colWeightWithWaste.Name = "colWeightWithWaste";
+            this.colWeightWithWaste.Visible = true;
+            this.colWeightWithWaste.VisibleIndex = 6;
+            // 
+            // colAdditionalMeasurement
+            // 
+            this.colAdditionalMeasurement.FieldName = "AdditionalMeasurement";
+            this.colAdditionalMeasurement.Name = "colAdditionalMeasurement";
+            this.colAdditionalMeasurement.Visible = true;
+            this.colAdditionalMeasurement.VisibleIndex = 7;
+            // 
+            // colAdditionalMeasurementWithWaste
+            // 
+            this.colAdditionalMeasurementWithWaste.FieldName = "AdditionalMeasurementWithWaste";
+            this.colAdditionalMeasurementWithWaste.Name = "colAdditionalMeasurementWithWaste";
+            this.colAdditionalMeasurementWithWaste.Visible = true;
+            this.colAdditionalMeasurementWithWaste.VisibleIndex = 8;
+            // 
+            // colTypeAdditionalMeasurement
+            // 
+            this.colTypeAdditionalMeasurement.FieldName = "TypeAdditionalMeasurement";
+            this.colTypeAdditionalMeasurement.Name = "colTypeAdditionalMeasurement";
+            this.colTypeAdditionalMeasurement.Visible = true;
+            this.colTypeAdditionalMeasurement.VisibleIndex = 9;
+            // 
+            // colNote
+            // 
+            this.colNote.FieldName = "Note";
+            this.colNote.Name = "colNote";
+            this.colNote.Visible = true;
+            this.colNote.VisibleIndex = 10;
+            // 
+            // StandartForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1107, 499);
+            this.Controls.Add(this.gridControl1);
+            this.Controls.Add(this.expandCollapsePanel1);
+            this.Controls.Add(this.panel1);
+            this.Name = "StandartForm";
             this.Text = "StandartForm";
+            ((System.ComponentModel.ISupportInitialize)(this.gridControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.gridView1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.positionStandartBindingSource)).EndInit();
+            this.ResumeLayout(false);
+
         }
 
         #endregion
+
+        private DevExpress.XtraGrid.GridControl gridControl1;
+        private System.Windows.Forms.BindingSource positionStandartBindingSource;
+        private DevExpress.XtraGrid.Views.Grid.GridView gridView1;
+        private DevExpress.XtraGrid.Columns.GridColumn colProfile;
+        private DevExpress.XtraGrid.Columns.GridColumn colStandartSize;
+        private DevExpress.XtraGrid.Columns.GridColumn colGostOnSort;
+        private DevExpress.XtraGrid.Columns.GridColumn colMarkSteal;
+        private DevExpress.XtraGrid.Columns.GridColumn colUtilizationRatio;
+        private DevExpress.XtraGrid.Columns.GridColumn colWeight;
+        private DevExpress.XtraGrid.Columns.GridColumn colWeightWithWaste;
+        private DevExpress.XtraGrid.Columns.GridColumn colAdditionalMeasurement;
+        private DevExpress.XtraGrid.Columns.GridColumn colAdditionalMeasurementWithWaste;
+        private DevExpress.XtraGrid.Columns.GridColumn colTypeAdditionalMeasurement;
+        private DevExpress.XtraGrid.Columns.GridColumn colNote;
+        private MakarovDev.ExpandCollapsePanel.ExpandCollapsePanel expandCollapsePanel1;
+        private System.Windows.Forms.Panel panel1;
     }
 }
