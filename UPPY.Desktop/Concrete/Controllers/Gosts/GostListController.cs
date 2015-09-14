b53ac310809dc89dbd9d627ef778ca5f07db2d8d
ctukc <ctukc.nt@gmail.com>
2015-09-14 13:24:55 +0300

    реализация контроллера гостов

--- UPPY.Desktop/Concrete/Controllers/Gosts/GostListController.cs
+++ UPPY.Desktop/Concrete/Controllers/Gosts/GostListController.cs
@@ -1,36 +1,53 @@
 ﻿using System;
 using System.Collections.Generic;
 using Core.DomainModel;
+using Core.Interfaces;
+using UPPY.Desktop.Interfaces.Controllers.Common;
 using UPPY.Desktop.Interfaces.Controllers.Gosts;
 
 namespace UPPY.Desktop.Concrete.Controllers.Gosts
 {
     public class GostListController : IGostListController
     {
+        private readonly IUppyControllersFactory _factory;
+        private readonly IClassDataManager<Gost> _gostDataManager;
+
+        public GostListController(IUppyControllersFactory factory, IClassDataManager<Gost> gostDataManager)
+        {
+            _factory = factory;
+            _gostDataManager = gostDataManager;
+        }
+
         public List<Gost> GetGostsList()
         {
-            throw new NotImplementedException();
+            return _gostDataManager.GetListCollection();
         }
 
         public Gost CreateDocument()
         {
-            throw new NotImplementedException();
+            return new Gost();
         }
 
         public void Save(Gost document)
         {
-            throw new NotImplementedException();
+            _gostDataManager.InsertOrUpdate(document);
         }
 
         public void Delete(Gost document)
         {
-            throw new NotImplementedException();
+            _gostDataManager.Delete(document);
         }
 
         public event EventHandler<EventArgs> DataRefreshed;
+
         public void ShowGostInAnotherView(Gost gost)
         {
-            throw new NotImplementedException();
+            var docController = _factory.GetDocumentController<Gost>();
+            docController.Document = gost;
+            if (docController.ShowViewDialog())
+            {
+                Save(gost);
+            }
         }
     }
 }
