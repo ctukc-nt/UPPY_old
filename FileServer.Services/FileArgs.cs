68e56ef18073347fd8381e484aa672e4138f257e
ctukc <ctukc.nt@gmail.com>
2015-09-15 10:02:31 +0300

    добавлен сервис хранения файлов

---
+++ FileServer.Services/FileArgs.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+
+namespace FileServer.Services
+{
+	public delegate void FileEventHandler(object sender, FileEventArgs e);
+
+	public class FileEventArgs : EventArgs
+	{
+		/// <summary>
+		/// Gets the virtual path.
+		/// </summary>
+		public string VirtualPath { get; } = null;
+
+	    /// <summary>
+		/// Initializes a new instance of the <see cref="FileEventArgs"/> class.
+		/// </summary>
+		/// <param name="vPath">The v path.</param>
+		public FileEventArgs(string vPath)
+		{
+			this.VirtualPath = vPath;
+		}
+	}
+}
---
+++ FileServer.Services/FileUploadMessage.cs
@@ -0,0 +1,15 @@
+﻿using System.IO;
+using System.ServiceModel;
+
+namespace FileServer.Services
+{
+    [MessageContract]
+    public class FileUploadMessage
+    {
+        [MessageHeader(MustUnderstand = true)]
+        public string VirtualPath { get; set; }
+
+        [MessageBodyMember(Order = 1)]
+        public Stream DataStream { get; set; }
+    }
+}
